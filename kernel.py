ee2264554eca87952e0360e2d3cef04f78b5e608
lexanic37 <lexanic37@gmail.com>
2018-10-19 18:32:31 +0300

    add import nmap scan, and workspace manager

--- kernel.py
+++ kernel.py
@@ -4,20 +4,21 @@ from msfModules.pre_module import *
 
 
 class Kernel():
-    def __init__(self, name_workspace, scope, path_to_scan):
+    def __init__(self, name_workspace, path_to_scan, scope):
         self.config = self.get_config()
         self.msfrpc_client = self.connect_to_msfrpc()
 
-        # TODO Сделать что когда имя воркспейса тоже то подтянуть старый воркспейс а не создавать новый
+        # TODO make check on created workspace
         self.workspace = self.get_workspace(name_workspace)
 
-        self.pre_module = preModule(self.msfrpc_client, self.workspace)
         self.data_to_modules = self.parse_config()
 
         self.import_scan_result(path_to_scan)
+        self.pre_module = preModule(self.msfrpc_client, self.workspace)
 
-        #for specify manually
-        # self.scope = scope
+        # for specify manually
+        self.scope = scope
+        print(self.msfrpc_client.jobs)
         # self.scope_list = self.generate_scope(scope)
 
     def get_config(self):
@@ -40,11 +41,12 @@ class Kernel():
         return data_to_modules
 
     def run_pre_module(self):
+
         print self.data_to_modules
         for port, data_port in self.data_to_modules.iteritems():
             for exp_with_commands in data_port['pre-modules']:
                 for exploit, commands in exp_with_commands.iteritems():
-                    self.pre_module.run( exploit, commands['commands'], port)
+                    self.pre_module.run(exploit, commands['commands'], port,self.generate_scope_for_port(port))
 
     def connect_to_msfrpc(self):
         client = MsfRpcClient(str(self.config['msfrpc']['password']))
@@ -59,20 +61,27 @@ class Kernel():
 
         return workspace
 
+    def generate_scope_for_port(self, port):
+        ip_list = ''
+        services = self.workspace.services.find(port=port)
+        for service in services:
+            ip_list += ' ' + service['host']
+
+        return ip_list
 
     def import_scan_result(self, path):
         self.workspace.importfile(path)
 
-    # def generate_scope(self, scope):
-    #     '''Return IPs in IPv4 range, inclusive.'''
-    #     start = scope.split('-')[0]
-    #     end = scope.split('-')[1]
-    #     start_int = int(ip_address(start).packed.encode('hex'), 16)
-    #     end_int = int(ip_address(end).packed.encode('hex'), 16)
-    #     return [ip_address(ip).exploded for ip in range(start_int, end_int)]
+        # def generate_scope_list(self, scope):
+        #     '''Return IPs in IPv4 range, inclusive.'''
+        #     start = scope.split('-')[0]
+        #     end = scope.split('-')[1]
+        #     start_int = int(ip_address(start).packed.encode('hex'), 16)
+        #     end_int = int(ip_address(end).packed.encode('hex'), 16)
+        #     return [ip_address(ip).exploded for ip in range(start_int, end_int)]
 
 
-kernel = Kernel('deployment','scan_nmap' , u'192.168.1.240-192.168.2.5')
+kernel = Kernel('deployment', 'scan_nmap', '')
 
 kernel.run_pre_module()
 # kernel.create_workspace()
--- msfModules/pre_module.py
+++ msfModules/pre_module.py
@@ -5,21 +5,39 @@ from metasploit.msfrpc import MsfRpcClient
 
 
 class preModule():
-    def __init__(self, msf_rpc_client, ):
+    def __init__(self, msf_rpc_client, workspace):
         self.msf_client = msf_rpc_client
         self.workspace = workspace
 
-    def run(self, module_name, commands, scope, port):
+    def run(self, module_name,  commands, port, scope):
         splited_module = module_name.split('/', 1)
 
         module = self.msf_client.modules.use(splited_module[0], splited_module[1])
+
         for command in commands:
             for option, value in command.iteritems():
-                if value=='<RHOSTS>':
-                    value = value.replace('<RHOSTS>', scope)
-                elif value=='<RPORT>':
+                if value == '<RPORT>':
                     value = value.replace('<RPORT>', str(port))
-                module[option] = value
-                # print module[option]
 
-        print module.execute()
+                module[option] = value
+        module["RHOSTS"]=scope
+        print(module.execute())
+
+        print(self.msf_client.jobs.info('7'))
+
+
+    def run_manual(self, scope, port, commands):
+        for command in commands:
+            for option, value in command.iteritems():
+                if value == '<RHOSTS>':
+                    value = value.replace('<RHOSTS>', scope)
+                elif value == '<RPORT>':
+                    value = value.replace('<RPORT>', str(port))
+
+
+
+
+
+class JobManager():
+    def jobs(self):
+        return {preModule:[{'exploit': 17-010, 'machine': [{'ip':'1.1.1.1', 'job':'1'}]}]}
